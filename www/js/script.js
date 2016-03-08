257eaa261f27676809d1117963370783e9b34b8b
Craig Wells <cwells@northplains.com>
2016-03-08 14:50:17 +0000

    Animating line and added request frame handler

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,39 @@
+﻿// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ 
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+ 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 $(function () {
 
     //gloabl definitions
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+   // var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    var requestAnimationFrame = window.requestAnimationFrame;
     var canvas = document.getElementById('my-canvas');
     var context = canvas.getContext('2d');
 
@@ -9,49 +41,50 @@ $(function () {
     canvas.height = 500;
 
     var lineDefaults = {
-        movePos: {
+
+        startPos: {
             x: 1000,
             y: 300
         },
-        linePos: {
+
+        endPos: {
             x: 900,
-            y: 300
+            y: 100
         },
+
         count:0,
-        end:300
-    }
-
-    function AnimateLine() {
-
-        context.beginPath();
-        context.moveTo(lineDefaults.movePos.x, lineDefaults.movePos.y);
-        context.lineTo(lineDefaults.linePos.x, lineDefaults.linePos.y);
-        context.stroke();
-        context.closePath();
-
-        lineDefaults.linePos.x-100;
-
-        if (lineDefaults.count < lineDefaults.end) {
-            requestAnimationFrame(function () {
-                AnimateLine();
-                lineDefaults.count++;
-            });
-        }else{
-            reset();
-        }
-    }
-
-    function reset(){
-        clear();
-        lineDefaults.count = 0;
-        lineDefaults.linePos.x = 0;
-        AnimateLine();
+        end:900
     }
 
     function clear() {
         context.clearRect(0, 0, canvas.width, canvas.height);
     }
 
-    AnimateLine();
+    function Animate(){
+        if (lineDefaults.count < lineDefaults.end) {
+            requestAnimationFrame(function () {
+                clear();
+                drawline();
+                updatePositions();
+                lineDefaults.count++;
+                Animate();
+            });
+        }
+    };
+
+    Animate();
+
+    function updatePositions(){
+        lineDefaults.startPos.x -= 1;
+        lineDefaults.endPos.x -= 1;
+    }
+
+    function drawline(){
+        context.beginPath();
+        context.moveTo(lineDefaults.startPos.x, lineDefaults.startPos.y);
+        context.lineTo(lineDefaults.endPos.x, lineDefaults.endPos.y);
+        context.stroke();
+        context.closePath();
+    }
 
 });
